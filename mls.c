0c3738aef403d5cc5a934e06326fd6ca7c346fac
G. David Butler <gdavidbutler@users.noreply.github.com>
2024-07-11 00:47:54 +0000

    add GPL header

--- mls.c
+++ mls.c
@@ -1,6 +1,24 @@
-#include "mls.h"
+/*
+ * MerkleLamportSignature - a small Merkle signature scheme using Lamport signatures implementation
+ * Copyright (C) 2020-2024 G. David Butler <gdb@dbSystems.com>
+ *
+ * This file is part of MerkleLamportSignature
+ *
+ * MerkleLamportSignature is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MerkleLamportSignature is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
-/* Merkle Lamport signature */
+#include "mls.h"
 
 mlsSz_t
 mlsPrSz(
